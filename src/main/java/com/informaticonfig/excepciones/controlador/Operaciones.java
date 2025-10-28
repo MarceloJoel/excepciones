1f674e35df8337f70535ea673f87312d1141854d
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-10-28 11:31:17 -0500

    feat: add NullPointerException

--- src/main/java/com/informaticonfig/excepciones/controlador/Operaciones.java
+++ src/main/java/com/informaticonfig/excepciones/controlador/Operaciones.java
@@ -11,16 +11,18 @@ public class Operaciones {
 
     @GetMapping
     public String Divide(@RequestParam String numero) {
-        try {
-            int valor = Integer.parseInt(numero);
-            int resultado = 20 / valor;
-            return "Resultado: " + resultado;
-        } catch (NumberFormatException ERROR) {    //Formato de valor equivocado
-            return "Error: el valor ingresado ('" + numero + "') no es un numero";
-        } catch (ArithmeticException ERROR) {      //Excepcion aritmetico
-            return "Error: no se puede dividir entre 0";
-        }
-
+        int valor = Integer.parseInt(numero);
+        int resultado = 20 / valor;
+        return "Resultado: " + resultado;
     }
+}
 
+@RestController
+class ObjectoNulo {
+
+    @GetMapping("valornulo")
+    public String nulo() {
+        String valorNulo = null;
+        return "Valor nulo en la variable";
+    }
 }
--- src/main/java/com/informaticonfig/excepciones/manejador/ManejoErrores.java
+++ src/main/java/com/informaticonfig/excepciones/manejador/ManejoErrores.java
@@ -13,4 +13,14 @@ public class ManejoErrores {
         return new ResponseEntity<>("Error: division entre cero no permitida", HttpStatus.BAD_REQUEST);
     }
 
+    @ExceptionHandler(NullPointerException.class)
+    public ResponseEntity<String> handlerNullPointerException(NullPointerException ERROR) {
+        return new ResponseEntity<>("Error: valor de la variable no debe ser nulo", HttpStatus.BAD_REQUEST);
+    }
+
+    @ExceptionHandler(NumberFormatException.class)
+    public ResponseEntity<String> handlerNumberFormatException(NumberFormatException ERROR) {
+        return new ResponseEntity<>("Error: formato de valor ingresado es incorrecro", HttpStatus.BAD_REQUEST);
+    }
+
 }
