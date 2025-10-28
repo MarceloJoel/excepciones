575dd4cb2fcfe9fef7dc042747b39c8193ff0284
MarceloJoel <marcelorodriguez0369@gmail.com>
2025-10-28 11:22:44 -0500

    feat: add NumberFormatException

--- src/main/java/com/informaticonfig/excepciones/controlador/Operaciones.java
+++ src/main/java/com/informaticonfig/excepciones/controlador/Operaciones.java
@@ -2,16 +2,25 @@ package com.informaticonfig.excepciones.controlador;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/api/division")
+@RequestMapping("/api/divide")
 public class Operaciones {
 
     @GetMapping
-    public String Divide() {
-        int valor = 1;
-        return "resultado: " + valor;
+    public String Divide(@RequestParam String numero) {
+        try {
+            int valor = Integer.parseInt(numero);
+            int resultado = 20 / valor;
+            return "Resultado: " + resultado;
+        } catch (NumberFormatException ERROR) {    //Formato de valor equivocado
+            return "Error: el valor ingresado ('" + numero + "') no es un numero";
+        } catch (ArithmeticException ERROR) {      //Excepcion aritmetico
+            return "Error: no se puede dividir entre 0";
+        }
+
     }
 
 }
